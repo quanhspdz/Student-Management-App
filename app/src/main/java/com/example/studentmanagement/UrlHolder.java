eb846f2a7332c7b64b1a2ac3b9f22d89a341305c
quanhspdz <pc.quanhdz@gmail.com>
2021-09-27 21:50:09 +0700

    host app mysql database to free hostinger

--- app/src/main/java/com/example/studentmanagement/UrlHolder.java
+++ app/src/main/java/com/example/studentmanagement/UrlHolder.java
@@ -2,9 +2,9 @@ package com.example.studentmanagement;
 
 public class UrlHolder {
 
-    public final static String deleteUrl = "http://192.168.31.214/android-webservice/deleteData.php";
-    public final static String editUrl = "http://192.168.31.214/android-webservice/updateData.php";
-    public final static String addUrl = "http://192.168.31.214/android-webservice/insertData.php";
-    public final static String getDataUrl = "http://192.168.31.214/android-webservice/getData.php";
+    public final static String deleteUrl = "https://student-management-quanhspdz.000webhostapp.com/deleteData.php";
+    public final static String editUrl = "https://student-management-quanhspdz.000webhostapp.com/updateData.php";
+    public final static String addUrl = "https://student-management-quanhspdz.000webhostapp.com/insertData.php";
+    public final static String getDataUrl = "https://student-management-quanhspdz.000webhostapp.com/getData.php";
 
 }
